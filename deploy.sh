4f4ced9b691ee082555b2e794d835525b27f4857
barlowluo <luoweime@gmail.com>
2016-03-28 18:01:05 +0800

    [luowei] added deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+ssh -i ~/.ssh/deploy_rsa folk@101.200.122.163 bash -c "'
+cd folk-wechat
 git pull origin master
+source ~/.nvm/nvm.sh 
 npm install
 netstat -ntpl | grep 3001 &> /dev/null
-if [[ $? == 0  ]];then
+if [[ $? == 0 ]];then
     NODE_ENV=production pm2 reload /home/folk/folk-wechat/index.js -i 2 --name folk-wechat
 else
     NODE_ENV=production pm2 start /home/folk/folk-wechat/index.js  -i 2 --name folk-wechat
 fi
+'"
