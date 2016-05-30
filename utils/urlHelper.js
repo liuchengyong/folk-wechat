73705197c95b93c40f3ada3b108ca2084b18498b
诚勇刘 <liuchengyong@iluoteng.com>
2016-05-30 20:15:50 +0800

    [liu] add request headers options osType and code

--- utils/urlHelper.js
+++ utils/urlHelper.js
@@ -7,6 +7,10 @@ exports.createCouponUrl = (user, pid, mobile) => {
   return {
     url: `${config.apiUrl}${config.couponAPI}${pid}`,
     method: 'POST',
+    headers:{
+      'osType':'WEB',
+      'code':25
+    },
     form: {
       'openId': user.openid,
       'accessToken': user.access_token,
@@ -19,6 +23,10 @@ exports.createSocialUrl = (user) => {
   return {
     url: `${config.apiUrl}${config.socialAPI}`,
     method: 'POST',
+    headers:{
+      osType:'WEB',
+      code:25
+    },
     form: {
       type: 'WEIXIN_PUBLIC_ACCOUNT',
       socialId: user.openid,
